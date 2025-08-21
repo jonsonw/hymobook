b55dc592b67df3a2b394453f090e9a60e452e53d
jonsonw <jonsonw@qq.com>
2025-08-21 16:56:59 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "hymobook.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
